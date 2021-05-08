f9b39e33708ff85ab33c58ae43c99753014bfa3b
A <itthisakdr@gmail.com>
2021-05-08 19:53:20 +0700

    FINAL PRESENTATION

--- routes/ratesRoute.js
+++ routes/ratesRoute.js
@@ -3,7 +3,7 @@ const ratesController = require("../controllers/ratesController");
 const staffController = require("../controllers/staffController");
 const router = express.Router();
 
-router.get("/", staffController.protectUser, ratesController.getRates);
-router.put("/", staffController.protectUser, ratesController.updateRates);
+router.get("/", staffController.protectAdmin, ratesController.getRates);
+router.put("/", staffController.protectAdmin, ratesController.updateRates);
 
 module.exports = router;
